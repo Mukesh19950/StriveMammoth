b2f3cbdd6acb931fdcf484f88aefbccb0ab7ee57
mukes <mukes@Mukesh>
2023-02-01 19:57:56 +0530

    Changes

--- StriveMammoth/src/main/java/com/strivemammoth/pageobjects/ClientSearch.java
+++ StriveMammoth/src/main/java/com/strivemammoth/pageobjects/ClientSearch.java
@@ -2,25 +2,46 @@ package com.strivemammoth.pageobjects;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class ClientSearch {
+import com.strivemammoth.actiondriver.ActionClass;
+import com.strivemammoth.base.BaseClass;
 
+public class ClientSearch extends BaseClass {
+
+	ActionClass action = new ActionClass();
 	
 	@FindBy(xpath="//a[text()='Admin']")
 	private WebElement admin;
 	
 	
+	@FindBy(xpath = "//a[@href='#/admin/vehicle']")
+	private WebElement vehicle;
+	
+	@FindBy(xpath = "//input[@id='inputSearchbyName']")
+	private WebElement vehiclesearch;
+	
+	
+	public ClientSearch() {
+		PageFactory.initElements(getDriver(), this);
+	}
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void searchclient() throws Exception
+	{
+		
+		action.moveToElement(getDriver(), admin);
+		action.mouseover(getDriver(), admin);
+		action.click(getDriver(), admin);
+		Thread.sleep(2000);
+		action.click(getDriver(), vehicle);
+		Thread.sleep(4000);
+		action.type(vehiclesearch, "MUKESH");
+		Thread.sleep(4000);
+		
+		
+	}
 	
 	
 	
---
+++ StriveMammoth/src/test/java/com/strivemammoth/testcases/ClientSearch_TestCase.java
@@ -0,0 +1,70 @@
+package com.strivemammoth.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ClientSearch_TestCase {
+	
+	
+	
+	
+	
+	@BeforeMethod
+	public void setup()
+	{
+		
+		
+	}
+	
+	
+	
+	@Test
+	public void clientsearch()
+	{
+		
+		
+	}
+	
+	
+	
+	@AfterMethod
+	public void close()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
